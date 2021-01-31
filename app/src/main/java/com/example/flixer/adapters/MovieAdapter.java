cd7c894faf9e9fad05dc90f4f38b441f81d82447
TechZech <benzech7900@gmail.com>
2021-01-30 23:43:21 -0500

    landscape mode now works

--- app/src/main/java/com/example/flixer/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixer/adapters/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.example.flixer.adapters;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -66,7 +67,16 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
-            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+            String imageUrl;
+            if (context.getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
+                // landscape
+                imageUrl = movie.getBackdropPath();
+            }
+            else{
+                imageUrl = movie.getPosterPath();
+            }
+
+            Glide.with(context).load(imageUrl).into(ivPoster);
         }
     }
 
