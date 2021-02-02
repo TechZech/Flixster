ee946424291e94b621e24e592d6f9b0d2c114e9e
TechZech <benzech7900@gmail.com>
2021-02-01 21:44:52 -0500

    youtube trailer + autoload popular trailers

--- app/src/main/java/com/example/flixer/models/Movie.java
+++ app/src/main/java/com/example/flixer/models/Movie.java
@@ -3,17 +3,23 @@ package com.example.flixer.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
 
+    int movieId;
     String backdropPath;
     String posterPath;
     String title;
     String overview;
+    double rating;
 
+    // empty constructor needed by the parceler library
+    public Movie(){}
 
     public Movie(JSONObject jsonObject) throws JSONException {
 
@@ -21,7 +27,8 @@ public class Movie {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
-
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -48,7 +55,11 @@ public class Movie {
         return title;
     }
 
-    public String getOverview() {
-        return overview;
+    public String getOverview() { return overview; }
+
+    public double getRating() {
+        return rating;
     }
+
+    public int getMovieId() { return movieId; }
 }
